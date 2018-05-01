97f3dc75ef8e728fd63ff43bece5a274476ce951
Aaron19960821 <wyc8094@gmail.com>
2018-05-01 21:50:46 +0800

    Initial Commit

---
+++ mitGraphics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mitGraphics
+//
+//  Created by Yuchen Wang on 01/05/2018.
+//  Copyright © 2018 Yuchen Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
