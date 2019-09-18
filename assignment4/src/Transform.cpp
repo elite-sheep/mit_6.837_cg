c4498652ac7853d9b1bb23464b181d28b6cdc576
Yuchen Wang <wyc8094@gmail.com>
2019-09-18 14:58:05 +0800

    fix_transform_error

--- assignment4/src/Transform.cpp
+++ assignment4/src/Transform.cpp
@@ -16,13 +16,12 @@ Transform::Transform(const Matrix4f& m, Object3D* obj) :
 bool Transform::intersect(const Ray &r, Hit &h, float tmin) {
   Matrix4f transformInv = transform.inverse();
 
-  Vector3f newRayOrigin = (transform * Vector4f(r.getOrigin(), 1.0f)).xyz();
-  Vector3f newRayDir = (transform * Vector4f(r.getDirection(), 0.0f)).xyz();
-  newRayDir = newRayDir.normalized();
-  Ray newRay(newRayOrigin, newRayDir);
+  Vector3f newRayOrigin = (transformInv * Vector4f(r.getOrigin(), 1.0f)).xyz();
+  Vector3f newRayDir = (transformInv * Vector4f(r.getDirection(), 0.0f)).xyz();
+  Ray newRay(newRayOrigin, newRayDir.normalized());
 
   if (o->intersect(newRay, h, tmin)) {
-    Vector3f normal = (transformInv * Vector4f(h.getNormal(), 0.0)).xyz();
+    Vector3f normal = (transformInv.transposed() * Vector4f(h.getNormal(), 0.0)).xyz();
     normal = normal.normalized();
     h.set(h.getT(), h.getMaterial(), normal);
     return true;
